da4a483dcd66abbf66b92ca982b1e7e46b40f619
Eze Sunday <rexeze@gmail.com>
2016-03-20 19:04:33 +0000

    Update footer.php

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-10 col-lg-offset-1 text-center">
-                    <h4><strong>Start Bootstrap</strong>
+                    <h4><strong>Lorem Ipsum</strong>
                     </h4>
                     <p>3481 Melrose Place<br>Beverly Hills, CA 90210</p>
                     <ul class="list-unstyled">
@@ -24,7 +24,7 @@
                         </li>
                     </ul>
                     <hr class="small">
-                    <p class="text-muted">Copyright &copy; Your Website 2014</p>
+                    <p class="text-muted">Copyright &copy; Your Website 2016</p>
                 </div>
             </div>
         </div>
